526eb8da15daf4c9e4f0af4ec500ac7763e9a3fc
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2022-09-12 12:06:56 +0200

    first steps in java

---
+++ First_steps_in_java/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+//import java.util.function.ToDoubleBiFunction;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+//
+//        double num1 = Double.parseDouble(scanner.nextLine());
+//        double num2 = Double.parseDouble(scanner.nextLine());
+
+
+        System.out.println(num1 - num2);
+
+//        String name = scanner.nextLine();
+
+//        System.out.print("Hello, ");
+//        System.out.print(name);
+//        System.out.print("!");
+
+//        String firstName = scanner.nextLine();
+//        String lastName = scanner.nextLine();
+//        int age = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.printf("Hello %s %S, are you %d-years old??", firstName, lastName, age);
+
+//        String a = scanner.nextLine();
+//        String b = scanner.nextLine();
+//
+//        System.out.println(a.equals(b));
+
+//        System.out.println(123456 % 100 == 5);
+
+
+//        System.out.println(!(5 == 5) && (4 + 1 == 5));
+
+//        int number = 101;
+//        if (number >= 1) {
+//            System.out.println("Larger than 1");
+//        }
+//        if (number <= 101) {
+//            System.out.println("Less than 101");
+//            System.out.println("Equal to 101");
+//        }
+
+//        String text = "SoftUni";
+//        int length = text.length();
+
+//        String text = "SoftUni";
+//        char letter = text.charAt(4);
+
+
+
+    }
+}
+
+
